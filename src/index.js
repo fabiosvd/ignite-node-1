00d7a7f57d7dc5175f4eb476dbc17407fdf0b4be
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-16 13:14:14 -0300

    should not be able to update a non existing todo :heavy_check_mark:

--- src/index.js
+++ src/index.js
@@ -72,6 +72,11 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo)
+    return response.status(404).json({
+      error: "Todo not found",
+    });
+
   todo.title = title;
   todo.deadline = new Date(deadline);
 
